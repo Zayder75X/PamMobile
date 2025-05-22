352acf3da22656f870ee2db1d489a406f042343a
Zayder75X <mestrespider17@gmail.com>
2025-05-22 09:49:02 -0300

    Add files via upload

---
+++ app/index.tsx
@@ -0,0 +1,45 @@
+import { Text, TouchableOpacity } from "react-native";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+
+export default function Index() {
+  const nome = "Walter";
+  return <View>
+<Text>Jogo da Velha</Text>
+<Text>Vencedor</Text>
+<View>
+    <View>
+        <TouchableOpacity>
+            <Text></Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+            <Text></Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+            <Text></Text>
+        </TouchableOpacity>
+    </View>
+    <View>
+    <TouchableOpacity>
+            <Text></Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+            <Text></Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+            <Text></Text>
+        </TouchableOpacity>
+    </View>
+    <View>
+    <TouchableOpacity>
+            <Text></Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+            <Text></Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+            <Text></Text>
+        </TouchableOpacity>
+    </View>
+</View>
+</View>
+}
